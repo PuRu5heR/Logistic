42214d9455faa9a8d937986525d1c841604c390d
AMIR <sajlaubaevamir@gmail.com>
2022-12-12 16:31:26 +0300

    init

--- src/my/by/services/Services.java
+++ src/my/by/services/Services.java
@@ -3,7 +3,7 @@ package my.by.services;
 import my.by.transport.Transport;
 
 public class Services {
-    public static Transport[] checkLoadCapacity(Transport[] transports, double load){
+    public static Transport[] checkLoadCapacity(Transport[] transports, double load){     //проверка на грузоподъёмность
         Transport[] checked = new Transport[transports.length];
         int i = 0;
         for (Transport transport : transports){
@@ -15,7 +15,7 @@ public class Services {
         return checked;
     }
 
-    public static Transport checkPrice(Transport[] transports){
+    public static Transport checkPrice(Transport[] transports){                           //нахождения дешёвого варианта
         Transport lowestPrice = transports[0];
         for (Transport transport : transports){
             if (transport.getTransportationPrice() < lowestPrice.getTransportationPrice()){
@@ -25,7 +25,7 @@ public class Services {
         return lowestPrice;
     }
 
-    public static Transport checkSpeed(Transport[] transports){
+    public static Transport checkSpeed(Transport[] transports){                           //нахождение быстрого варианта
         Transport fastest = transports[0];
         for (Transport transport : transports){
             if (transport.getSpeed() > fastest.getSpeed()){
@@ -35,7 +35,7 @@ public class Services {
         return fastest;
     }
 
-    public static Transport[] checkPassengers(Transport[] transports, int passengers){
+    public static Transport[] checkPassengers(Transport[] transports, int passengers){   //проверка на вместимость пассажиров
         Transport[] checked = new Transport[transports.length];
         int i = 0;
         for (Transport transport : transports){
@@ -47,7 +47,7 @@ public class Services {
         return checked;
     }
 
-    public static Transport[] findingBestTransport(Transport[] transports, double load, int passengers){
+    public static Transport[] findingBestTransport(Transport[] transports, double load, int passengers){  //нахождение лучших вариантов для перевозки
         Transport[] checkedLoad = checkLoadCapacity(transports, load);
         Transport[] checkedPassengers = checkPassengers(checkedLoad, passengers);
         Transport fastestTransport = checkSpeed(checkedPassengers);
